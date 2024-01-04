acc0ffdc2432eb48ff7ebc0916e12f503a3e83cb
Denis Kozarezov <52127090+DenisKozarezov@users.noreply.github.com>
2024-01-04 18:48:44 +0300

    dev09

---
+++ develop/dev09/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func wget(url string) error {
+	response, err := http.Get(url)
+	defer response.Body.Close()
+
+	if err != nil {
+		return fmt.Errorf("Could not make a GET-query to %s. Reason: %w", url, err)
+	}
+
+	reader := bufio.NewReader(response.Body)
+	for {
+		string, err := reader.ReadString('\n')
+		fmt.Println(string)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	err := wget("https://www.wildberries.ru/")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
